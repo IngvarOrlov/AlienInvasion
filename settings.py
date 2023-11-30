edb9396fcc998219bfce84e1a4e746f1d1e6d054
IngvarOrlov <kampaiski@gmail.com>
2023-11-30 17:35:13 +0300

    Starting project

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+	def __init__(self):
+		self.screen_width = 800
+		self.screen_height = 600
+		self.bg_color = (10, 10, 10)
+		self.ship_speed = 0.9
+
+		self.bullet_speed = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60 ,60)
---
+++ ship.py
@@ -0,0 +1,24 @@
+import pygame
+class Ship():
+	def __init__(self,ai_game):
+		self.screen = ai_game.screen #экран присваивается атрибуту Ship
+		self.settings = ai_game.settings #создается атрибут дляиспользования в update
+		self.screen_rect = ai_game.screen.get_rect() #программа обращается к атрибуту rect обьекта экрана при помощи метода get_rect()
+		self.image = pygame.image.load('images/ship_small.png') # метод pygame.image.load() возвращает поверхность представляющую корабль которая присваивается self.image
+		self.rect = self.image.get_rect() #программа вызывает get_reсt() для получения атрибута rect поверхности корабля
+		self.rect.midbottom = self.screen_rect.midbottom #задаетсятекущая позиция прямоугольника корабля
+		self.x =float(self.rect.x) #сохранение вещественной координаты корабля
+		#Флаги перемещения
+		self.moving_right = False
+		self.moving_left = False
+	def update(self):
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		if self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+		self.rect.x = self.x	
+	def blitme(self):
+		self.screen.blit(self.image, self.rect) #рисует корабль в текущей позиции
+		
+
+#print (ai.settings.bg_color)		
